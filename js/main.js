c9b1d0c03014bb17c58572221e98586d5d433663
ahmar7 <ahmarjabbar7@gmail.com>
2022-05-22 13:33:37 +0500

    update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 let mblnav = () => {
-    console.log("aRUnnnd");
+    let navbar = document.getElementById("nav-mbl")
+    let openbar = document.getElementById("open-bar")
+    let closebar = document.getElementById("close-bar")
+    openbar.classList.toggle("hide")
+    navbar.classList.toggle("show-nav")
+    closebar.classList.toggle("hide-x")
+    // closebar.classList.remove("hide-x")
+}
+let closebar = () => {
+    let navbar = document.getElementById("nav-mbl")
+    let openbar = document.getElementById("open-bar")
+    openbar.classList.add("open")
+    console.log("Runninh");
+    navbar.classList.remove("show-nav")
+    // navbar.classList.add("hide-nav")
 }
